ea74d7af6b7f1e33c712ff7897d1fd4cecf4aab9
Adrian Alexander <dotakillingspree@yahoo.com>
2016-10-28 16:42:19 +0700

    3rd update

--- Cashier Machine.cpp
+++ Cashier Machine.cpp
@@ -14,8 +14,9 @@ int main()
 						   		 
 	int choice;
 	int a,b,c,d,e,f,g,h,i;
-	float total;
-	int sum;
+	float total[9] = {0,0,0,0,0,0,0,0,0};
+	float sum;
+	int counter;
 	
 	for(int i=0;i<9;i++)
 	{
@@ -34,84 +35,100 @@ int main()
 		cout<<"how many detergen you want to buy ";
 		cin>>a;
 		cout<<"you purchase "<<a<<" detergen";
-		total=a*6;
-		cout<<" and the Price is $"<<total<<endl<<endl;	
+		total[0]+=a*6;
+		cout<<" and the Price is $"<<total[0]<<endl<<endl;	
 	}
 	else if(choice==2)
 	{
 		cout<<"how many soap you want to buy ";
 		cin>>b;
 		cout<<"you just bought "<<b<<" soap";
-		total=b*5;
-		cout<<" and the Price is $"<<total<<endl<<endl;
+		total[1]+=b*5;
+		cout<<" and the Price is $"<<total[1]<<endl<<endl;
 	}
 	else if(choice==3)
 	{
 		cout<<"how many shampoo you want to buy ";
 		cin>>c;
 		cout<<"you just bought "<<c<<" shampoo";
-		total=c*3;
-		cout<<" and the price is $"<<total<<endl<<endl;
+		total[2]+=c*3;
+		cout<<" and the price is $"<<total[2]<<endl<<endl;
 	}
 	else if(choice==4)
 	{
 		cout<<"how many tooth paste you want to buy ";
 		cin>>d;
 		cout<<"you just bought "<<d<<" tooth paste";
-		total=d*2;
-		cout<<" and the price is $"<<total<<endl<<endl;
+		total[3]+=d*2;
+		cout<<" and the price is $"<<total[3]<<endl<<endl;
 	}
 	else if(choice==5)
 	{
 		cout<<"how many cocacola you want to buy ";
 		cin>>e;
 		cout<<"you just bought "<<e<<" cocacola";
-		total=e*1;
-		cout<<" and the price is $"<<total<<endl<<endl;
+		total[4]+=e*1;
+		cout<<" and the price is $"<<total[4]<<endl<<endl;
 	}
 	else if(choice==6)
 	{
 		cout<<"how many sprite you want to buy ";
 		cin>>f;
 		cout<<"you just bought "<<f<<" sprite";
-		total=f*1.5;
-		cout<<" and the price is $"<<total<<endl<<endl;
+		total[5]+=f*1.5;
+		cout<<" and the price is $"<<total[5]<<endl<<endl;
 	}
 	else if(choice==7)
 	{
 		cout<<"how many fanta you want to buy ";
 		cin>>g;
 		cout<<"you just bought "<<g<<" fanta";
-		total=g*1.75;
-		cout<<" and the price is $"<<total<<endl<<endl;
+		total[6]+=g*1.75;
+		cout<<" and the price is $"<<total[6]<<endl<<endl;
 	}
 	else if(choice==8)
 	{
 		cout<<"how many wax you want to buy ";
 		cin>>h;
 		cout<<"you just bought "<<h<<" wax";
-		total=h*7;
-		cout<<" and the price is $"<<total<<endl<<endl;
+		total[7]+=h*7;
+		cout<<" and the price is $"<<total[7]<<endl<<endl;
 	}
 	else if(choice==9)
 	{
 		cout<<"how many toys you want to buy ";
 		cin>>i;
 		cout<<"you just bought "<<i<<" toys";
-		total=i*8;
-		cout<<" and the price is $"<<total<<endl<<endl;
+		total[8]+=i*8;
+		cout<<" and the price is $"<<total[8]<<endl<<endl;
+	}
+	else
+	{
+		return 0;
 	}
 	
 	//string chooice;
-	next:
+	//next:
 		cout<<"any other item you want to buy ? Yes/No"<<" ";
 		string nextchoice;
 		cin>>nextchoice;
 		cout<<endl;
-	
+
 	if(nextchoice=="Y" || nextchoice=="y" || nextchoice=="yes" || nextchoice=="Yes")
 	{
 	goto chooice;	
+	}
+	else if(nextchoice=="n" || nextchoice=="N" || nextchoice=="no" || nextchoice=="No")
+	{  
+		for(int i =0; i <9; i++)
+		sum += total[i];
+		cout<<"the total price is $"<<sum<<endl;
+		
+	}
+	
+	if (sum>=100 && sum<=200)
+	{
+		cout<<"this is the total price after discount $"<<sum-(sum*0.1);
 	}
 	/* still confused how to sum all
 	else if(nextchoice=="N" || nextchoice=="n")
